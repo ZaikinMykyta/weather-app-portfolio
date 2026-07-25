3432edd61b0cf8a8222650b77bac7fdf4e730b59
ZaikinMykyta <zaikinmykyta@gmail.com>
2026-07-26 00:08:06 +0300

    add: changed the logo styling

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -7,7 +7,7 @@ const AppFooter = () => {
         <div className="flex px-[5vw] mt-[3vh] justify-between text-white bg-[#2F2F2F]">
             <div className="flex-col py-[2.5vh]">
                 <div>
-                    <Link to='/' className="text-2xl">Weather SPA</Link>
+                    <Link to='/' className="text-2xl">Weather <span className="text-3xl">SPA</span></Link>
                     <p className="text-sm">Your go-to weather information platform!</p>
                 </div>
                 <p className="mt-[4vh] text-[0.7em] brightness-80">WeatherSpa ©2026</p>
